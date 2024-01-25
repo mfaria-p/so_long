115071cf88a53e01ad39e3bc4fb60f4a433d63ef
mfaria-p <148826532+mfaria-p@users.noreply.github.com>
2024-01-25 00:01:04 +0000

    Update path_verifier.c

--- srcs/map_check/path_verifier.c
+++ srcs/map_check/path_verifier.c
@@ -12,4 +12,35 @@
 
 #include "../../inc/solong.h"
 
+void	mapflood(t_data *data)
+{
+	int		ne;
+	int		nc;
 
+	nc = 0;
+	ne = 1;
+	ft_flood(data, data->yp, data->xp, &ne, &nc);
+	if (data->n_colect != nc || ne != 0)
+		ft_exit("No valid path", EXIT_FAILURE, data);
+}
+
+char	**ft_flood(t_data *data, int yp, int xp, int *ne, int *nc)
+{
+	if (data->map[yp][xp] == '1')
+		return (data->map);
+	else if (data->map[yp][xp] == 'X')
+		return (data->map);
+	else
+	{
+		if (data->map[yp][xp] == 'E')
+			(*ne)--;
+		else if (data->map[yp][xp] == 'C')
+			(*nc)++;
+		data->map[yp][xp] = 'X';
+		ft_flood(data, (yp + 1), xp, ne, nc);
+		ft_flood(data, (yp - 1), xp, ne, nc);
+		ft_flood(data, yp, (xp + 1), ne, nc);
+		ft_flood(data, yp, (xp - 1), ne, nc);
+		return (data->map);
+	}
+}
