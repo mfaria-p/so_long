60e735c1575769c1d68ea594885b58aff21d191e
mfaria-p <148826532+mfaria-p@users.noreply.github.com>
2024-01-27 18:19:58 +0000

    Update moves.c

--- srcs/init/moves.c
+++ srcs/init/moves.c
@@ -10,4 +10,16 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../../inc/so_long.h"
 
+void right_move(t_data *data)
+{
+  data->moves++;
+  ft_printf("Moves: %i\n", data->moves);
+  mlx_put_image_to_window(data->mlx_ptr, data->win_ptr,
+			data->fimg, data->xp * SIZE, data->yp * SIZE);
+		data->xp++;
+		mlx_put_image_to_window(d->mlx_ptr, d->win_ptr,
+			d->pimg, d->xp * SIZE, d->yp * SIZE);
+  
+}
