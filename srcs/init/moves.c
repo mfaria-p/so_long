6ec16ec344dd1911ac305157e3eb9e7c1d28ad72
mfaria-p <148826532+mfaria-p@users.noreply.github.com>
2024-01-27 18:33:24 +0000

    Update moves.c

--- srcs/init/moves.c
+++ srcs/init/moves.c
@@ -14,12 +14,84 @@
 
 void right_move(t_data *data)
 {
-  data->moves++;
-  ft_printf("Moves: %i\n", data->moves);
-  mlx_put_image_to_window(data->mlx_ptr, data->win_ptr,
-			data->fimg, data->xp * SIZE, data->yp * SIZE);
-		data->xp++;
-		mlx_put_image_to_window(d->mlx_ptr, d->win_ptr,
-			d->pimg, d->xp * SIZE, d->yp * SIZE);
-  
+ 	data->moves++;
+ 	ft_printf("Moves: %i\n", data->moves);
+ 	mlx_put_image_to_window(data->mlx_ptr, data->win_ptr,
+		data->fimg, data->xp * SIZE, data->yp * SIZE);
+	data->xp++;
+	mlx_put_image_to_window(data->mlx_ptr, data->win_ptr,
+		data->pimg, data->xp * SIZE, data->yp * SIZE);
+	if (d->map[data->yp][data->xp] == 'C')
+	{
+		data->n_colect--;
+		data->map[data->yp][data->xp] = '0';
+	}
+	if (data->xp == data->xe && data->yp == data->ye && data->n_colect == 0)
+	{
+		ft_printf("Yey! Game over!\n");
+		ft_exit(NULL, EXIT_SUCCESS, data);
+	}  
+}
+
+void left_move(t_data *data)
+{
+ 	data->moves++;
+ 	ft_printf("Moves: %i\n", data->moves);
+ 	mlx_put_image_to_window(data->mlx_ptr, data->win_ptr,
+		data->fimg, data->xp * SIZE, data->yp * SIZE);
+	data->xp--;
+	mlx_put_image_to_window(data->mlx_ptr, data->win_ptr,
+		data->pimg, data->xp * SIZE, data->yp * SIZE);
+	if (d->map[data->yp][data->xp] == 'C')
+	{
+		data->n_colect--;
+		data->map[data->yp][data->xp] = '0';
+	}
+	if (data->xp == data->xe && data->yp == data->ye && data->n_colect == 0)
+	{
+		ft_printf("Yey! Game over!\n");
+		ft_exit(NULL, EXIT_SUCCESS, data);
+	}  
+}
+
+void up_move(t_data *data)
+{
+ 	data->moves++;
+ 	ft_printf("Moves: %i\n", data->moves);
+ 	mlx_put_image_to_window(data->mlx_ptr, data->win_ptr,
+		data->fimg, data->xp * SIZE, data->yp * SIZE);
+	data->yp--;
+	mlx_put_image_to_window(data->mlx_ptr, data->win_ptr,
+		data->pimg, data->xp * SIZE, data->yp * SIZE);
+	if (d->map[data->yp][data->xp] == 'C')
+	{
+		data->n_colect--;
+		data->map[data->yp][data->xp] = '0';
+	}
+	if (data->xp == data->xe && data->yp == data->ye && data->n_colect == 0)
+	{
+		ft_printf("Yey! Game over!\n");
+		ft_exit(NULL, EXIT_SUCCESS, data);
+	}  
+}
+
+void left_move(t_data *data)
+{
+ 	data->moves++;
+ 	ft_printf("Moves: %i\n", data->moves);
+ 	mlx_put_image_to_window(data->mlx_ptr, data->win_ptr,
+		data->fimg, data->xp * SIZE, data->yp * SIZE);
+	data->yp++;
+	mlx_put_image_to_window(data->mlx_ptr, data->win_ptr,
+		data->pimg, data->xp * SIZE, data->yp * SIZE);
+	if (d->map[data->yp][data->xp] == 'C')
+	{
+		data->n_colect--;
+		data->map[data->yp][data->xp] = '0';
+	}
+	if (data->xp == data->xe && data->yp == data->ye && data->n_colect == 0)
+	{
+		ft_printf("Yey! Game over!\n");
+		ft_exit(NULL, EXIT_SUCCESS, data);
+	}  
 }
